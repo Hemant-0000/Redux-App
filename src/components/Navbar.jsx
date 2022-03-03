586133c3388e69224b0861fefa4fa24ecd9f1a9a
Hemant <hrsmart2150@gmail.com>
2022-03-03 19:59:07 +0530

    making redux store named as state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
+                <div className="container-fluid">
                     <Link className="navbar-brand" to="/">State bank of Harry</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" to="/">Home</Link>
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Shop = () => {
   return (
     <div className='container my-3'>
-        <h2>Buy Adidas Shoes 50Rs</h2>
+        <h2>Deposit/Withdraw Money</h2>
         <button className="btn btn-primary mx-2 my-3">-</button>
-        Add to cart
+        Update Balance
         <button className="btn btn-primary mx-2 my-3">+</button>
     </div>
   )
